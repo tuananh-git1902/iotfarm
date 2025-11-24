8ca617f5e59e6e9975bdde8298f6fe4434e22ccb
tuananh-git1902 <tuananhbui190205@gmail.com>
2025-11-24 17:42:24 +0700

    push the code to github

---
+++ code.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+/* run this program using the console p
+
+int main(int argc, char** argv) {
+    count<<"Hello C++";
+    return 0;
+}
---
+++ iotfarm.py
@@ -0,0 +1,46 @@
+from time import sleep
+from SX127x.LoRa import *
+from SX127x.board_config import BOARD
+import sys
+import datetime
+
+BOARD.setup()
+
+class LoRaRcvCont(LoRa):
+    def __init__(self, verbose=False):
+        super(LoRaRcvCont, self).__init__(verbose)
+        self.set_mode(MODE.SLEEP)
+        self.set_dio_mapping([0] * 6)
+
+    def start(self):
+        self.reset_ptr_rx()
+        self.set_mode(MODE.RXCONT)
+        print("LoRa receiver started...")
+
+        try:
+            while True:
+                sleep(0.3)
+                sys.stdout.flush()
+        except KeyboardInterrupt:
+            print("\nStopping LoRa receiver...")
+            self.set_mode(MODE.SLEEP)
+            BOARD.teardown()
+
+    def on_rx_done(self):
+        print("\n====================================")
+        print("Packet received at", datetime.datetime.now())
+        print("====================================")
+        self.clear_irq_flags(RxDone=1)
+        payload = self.read_payload(nocheck=False)
+        try:
+            data = bytes(payload).decode("utf-8")
+        except:
+            data = bytes(payload).decode("latin-1", "ignore")
+        print("Raw data:", data)
+        print("Payload bytes:", payload)
+        print("------------------------------------")
+        print("Packet RSSI:", self.get_pkt_rssi_value(), "dBm")
+        print("Packet SNR :", self.get_pkt_snr_value(), "dB")
+        print("Frequency  :", self.get_freq() / 1e6, "MHz")
+        print("Length     :", len(payload), "bytes")
+        print("====================================\n")
